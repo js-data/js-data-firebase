c189349a3d67cab068435180105bf969db3f3b67
Jason Dobry <jason.dobry@gmail.com>
2014-10-02 15:50:25 -0600

    Example update.

--- example/example.js
+++ example/example.js
@@ -2,43 +2,35 @@
   angular.module('firebase-example', [])
     .factory('store', function () {
       var store = new JSData.DS();
+
       store.registerAdapter('firebase', new DSFirebaseAdapter({
         basePath: 'https://js-data-firebase.firebaseio.com'
       }), { default: true });
+
       return store;
     })
     .factory('User', function (store) {
       return store.defineResource('user');
     })
-    .controller('firebaseCtrl', function ($scope, $timeout, User) {
+    .controller('firebaseCtrl', function ($scope, User) {
       var fCtrl = this;
-      User.findAll().then(function () {
+
+      User.findAll().then(function (users) {
+        $scope.users = users;
         $scope.$apply();
       });
 
       $scope.add = function (user) {
-        $scope.creating = true;
         User.create(user).then(function () {
-          $scope.creating = false;
           fCtrl.name = '';
-          $timeout();
-        }, function () {
-          $scope.creating = false;
+          $scope.$apply();
         });
       };
+
       $scope.remove = function (user) {
-        $scope.destroying = user.id;
         User.destroy(user.id).then(function () {
-          delete $scope.destroying;
-          $timeout();
-        }, function () {
-          delete $scope.destroying;
+          $scope.$apply();
         });
       };
-      $scope.$watch(function () {
-        return User.lastModified();
-      }, function () {
-        $scope.users = User.filter();
-      });
     });
 })();
