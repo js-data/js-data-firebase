4433da44a940dc642f293d3ba09b93e4b86cb17a
Jason Dobry <jason.dobry@gmail.com>
2015-03-03 22:09:15 -0700

    Updated example.

--- examples/example.js
+++ examples/example.js
@@ -1,33 +1,46 @@
-(function () {
-  angular.module('firebase-example', ['js-data'])
-    .config(function (DSFirebaseAdapterProvider) {
-      DSFirebaseAdapterProvider.defaults.basePath = 'https://js-data-firebase.firebaseio.com';
-    })
-    .run(function (DS, DSFirebaseAdapter) {
-      // js-data-angular created a new store automatically and registered it as DS.
-      // The firebase adapter was already registered, but we want to make it the default.
-      DS.registerAdapter('firebase', DSFirebaseAdapter, { default: true });
-    })
-    .factory('User', function (DS) {
-      return DS.defineResource('user');
-    })
-    .controller('firebaseCtrl', function ($scope, User) {
-      var fCtrl = this;
+angular.module('firebase-example', ['js-data'])
+  .config(function (DSFirebaseAdapterProvider) {
+    var basePath = 'https://js-data-firebase.firebaseio.com';
+    DSFirebaseAdapterProvider.defaults.basePath = basePath;
+  })
+  .run(function (DS, DSFirebaseAdapter) {
 
-      User.findAll().then(function () {
-        $scope.users = User.filter();
+    // js-data-angular created a new store
+    // automatically and registered it as DS.
+    // The firebase adapter was already registered,
+    // but we want to make it the default.
+    DS.registerAdapter(
+      'firebase',
+      DSFirebaseAdapter,
+      { default: true }
+    );
+  })
+  .factory('User', function (DS) {
+    return DS.defineResource('user');
+  })
+  .controller('firebaseCtrl', function ($scope, User) {
+    var fCtrl = this;
+
+    // Pull the initial list of users
+    // from Firebase
+    User.findAll();
+
+    // Update the list of users on the
+    // scope whenever the collection
+    // changes
+    User.bindAll({}, $scope, 'users');
+
+    $scope.add = function (user) {
+      // Create a new user in Firebase
+      return User.create(user).then(function () {
+        fCtrl.name = '';
       });
+    };
 
-      User.bindAll({}, $scope, 'users');
+    $scope.remove = function (user) {
+      // Destroy a user from firebase
+      return User.destroy(user.id);
+    };
+  });
 
-      $scope.add = function (user) {
-        return User.create(user).then(function () {
-          fCtrl.name = '';
-        });
-      };
-
-      $scope.remove = function (user) {
-        return User.destroy(user.id);
-      };
-    });
-})();
+hljs.initHighlightingOnLoad();
